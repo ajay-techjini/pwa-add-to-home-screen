137db9994c7c958cf7bf49ff458000476c46b8b2
Ajay <ajay@techjini.com>
2019-04-03 15:59:16 +0530

    deployment

---
+++ docs/precache-manifest.7dbbb1fb55ae669d8eaebe68343bf4df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fea9c84cedd48127c78e",
+    "url": "https://ajay-techjini.github.io/pwa-add-to-home-screen/static/js/runtime~main.0f5a3722.js"
+  },
+  {
+    "revision": "f0c1bc7a1f418f56df5a",
+    "url": "https://ajay-techjini.github.io/pwa-add-to-home-screen/static/js/main.895b22d7.chunk.js"
+  },
+  {
+    "revision": "05969d6e38952f120878",
+    "url": "https://ajay-techjini.github.io/pwa-add-to-home-screen/static/js/2.d81d4a98.chunk.js"
+  },
+  {
+    "revision": "f0c1bc7a1f418f56df5a",
+    "url": "https://ajay-techjini.github.io/pwa-add-to-home-screen/static/css/main.9fcd074f.chunk.css"
+  },
+  {
+    "revision": "228c2a0e1b0099bacf6143760af3c908",
+    "url": "https://ajay-techjini.github.io/pwa-add-to-home-screen/index.html"
+  }
+];
