496e6fce97751a5576f8d12a874fc5a78c2a31ae
Ajay <ajay@techjini.com>
2019-04-03 11:30:18 +0530

    docs page

---
+++ docs/precache-manifest.97cf988eab9f9912011ce8279d90ce30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6d20c83f9d6f9b65d92e",
+    "url": "/static/js/main.82355831.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "6d20c83f9d6f9b65d92e",
+    "url": "/static/css/main.c07c5f34.chunk.css"
+  },
+  {
+    "revision": "7c02803d6b514c855084e97a97c3339c",
+    "url": "/index.html"
+  }
+];
