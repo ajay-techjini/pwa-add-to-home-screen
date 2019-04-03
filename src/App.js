ca32588dd5ce1183172a2b954cf6e4ed241d8be2
Ajay <ajay@techjini.com>
2019-04-03 15:58:19 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  deferredPrompt;
-  state = {
-    enablePopup: false
-  }
-  componentDidMount() {
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      this.deferredPrompt = e;
-      this.setState({enablePopup: true});
-    });
-  }
-
-  showAddToHomeScreenPopup = () => {
-    this.setState({
-      enablePopup: false
-    }, async () => {
-      // Show the prompt
-      this.deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      const choiceResult = await this.deferredPrompt.userChoice
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      this.deferredPrompt = null;
-    });
+  componentDidMount = () => {
+    window.document.querySelector('#show-add-to-home-screen-popup button')
+      .addEventListener('click', async () => {
+        window.document.getElementById('show-add-to-home-screen-popup').classList.remove('active');
+        // Show the prompt
+        window.deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        const choiceResult = await window.deferredPrompt.userChoice
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+          document.querySelector('.App').innerHTML = 'User accepted the A2HS prompt';
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        window.deferredPrompt = null;
+      });
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        {this.state.enablePopup && (
-          <div>
-            <div>Enable add to home screen popup</div>
-            <button onClick={this.showAddToHomeScreenPopup}>Enable</button>
-          </div>
-        )
-        }
+        
       </div>
     );
   }
