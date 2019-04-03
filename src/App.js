3736d3186dde7fc107d18a95c6e3c61716ed12df
Ajay <ajay@techjini.com>
2019-04-03 11:16:18 +0530

    added docs folder for testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  deferredPrompt;
+  componentDidMount() {
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
+  }
+
   render() {
     return (
       <div className="App">
